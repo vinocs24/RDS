41d60fae93874107f72ff141d9af3406f6a060c2
vinocs24 <47525526+vinocs24@users.noreply.github.com>
2019-12-02 01:03:58 +0530

    Update RDS.tf

--- RDS.tf
+++ RDS.tf
@@ -1,7 +1,7 @@
 resource "aws_db_subnet_group" "default" {
     name        = "wp-db-subnet-tf"
     description = "VPC Subnets"
-    subnet_ids  = ["${aws_subnet.wp-public-tf.id}", "${aws_subnet.wp-private-tf.id}"]
+    subnet_ids  = [aws_subnet.wp-public-tf.id, aws_subnet.wp-private-tf.id]
 }
 
 resource "aws_db_instance" "wordpress" {
@@ -10,14 +10,14 @@ resource "aws_db_instance" "wordpress" {
     engine                 = "mysql"
     engine_version         = "5.7.10"
     port                   = "3306"
-    instance_class         = "${var.db_instance_type}"
-    name                   = "${var.db_name}"
-    username               = "${var.db_user}"
-    password               = "${var.db_password}"
+    instance_class         = var.db_instance_type
+    name                   = var.db_name
+    username               = var.db_user
+    password               = var.db_password
     availability_zone      = "us-east-1b"
-    vpc_security_group_ids = ["${aws_security_group.wp-db-sg-tf.id}"]
+    vpc_security_group_ids = [aws_security_group.wp-db-sg-tf.id]
     multi_az               = false
-    db_subnet_group_name   = "${aws_db_subnet_group.default.id}"
+    db_subnet_group_name   = aws_db_subnet_group.default.id
     parameter_group_name   = "default.mysql5.7"
     publicly_accessible    = false
 }
